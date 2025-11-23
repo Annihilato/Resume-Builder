7717c5740924720a62125c95d05b3112b201e95e
Annihilato <104785167+Annihilato@users.noreply.github.com>
2025-11-23 13:27:42 +0530

    Main.java

--- Resume-Builder/Resume-Builder/src/main/java/com/resumebuilder/Main.java
+++ Resume-Builder/Resume-Builder/src/main/java/com/resumebuilder/Main.java
@@ -1,1 +1,12 @@
 // Placeholder for Main.java
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        // Start Swing GUI on Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            new ResumeGUI();
+        });
+    }
+}
